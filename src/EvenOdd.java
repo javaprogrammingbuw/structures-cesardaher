a3661aa234fce48167915bad246fbd9432740d2d
cesardaher <49917122+cesardaher@users.noreply.github.com>
2019-04-25 15:36:29 -0300

    Add files via upload
    Solution

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -6,7 +6,32 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
+		int x = 0;
+		int y = scan.nextInt();
+
+
+		do{
+			if (x % 2 == 0){
+				x++;
+
+			} else {
+				System.out.println(x);
+				x++;
+
+			}
+
+		}while(x <= y);
+
+
+
+		/*while (x % 2 == 0 && x < y){
+			System.out.println(x);
+			x++;
 		
+
+		}*/
+
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
