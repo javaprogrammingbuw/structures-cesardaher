783d73c91b7c3e47430ce03dbd5f2a11ea90c1e3
cesardaher <49917122+cesardaher@users.noreply.github.com>
2019-04-25 16:13:54 -0300

    Solution

--- src/SumMeanMinMax.java
+++ src/SumMeanMinMax.java
@@ -8,12 +8,31 @@ public class SumMeanMinMax {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		System.out.println("Enter three values.");
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
-		
+		int sum = a + b + c;
+		int mean = (a + b + c)/3;
+		int max = a;
+		int min = a;
+
 		System.out.println("a: " + a + " b: " + b + " c: " + c);
+		System.out.println("Sum:" + sum);
+		System.out.println("Mean:" + mean);
+
+		if (b >= max){max = b;}
+		if (c >= max){max = c;}
+
+		if (b <= min){min = b;}
+		if (c <= min){min = c;}
+
+		System.out.println("Max:" + max);
+		System.out.println("Min:" + min);
+
+
 
 		/*
 		 * Todo: Print the sum, mean, max Value and min Value of a, b and c.
